134d4b02245c42ef00a1c2ffe48bc6b2be3baa67
porcyFields <>
2021-06-17 21:49:23 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,86 @@
+var parar =0;
+while(parar == 0){
+
+console.log("Bem vindo ao sistema de Avaliação do Restaurante La Casa Tropicale!")
+console.log("")
+var usuario = prompt("Informe seu Usuario: ");
+var senha = prompt("Digite sua Senha: ");
+        
+        var usuarioCorreto = "Anderson";
+        var senhaCorreta = "1254";
+
+if(usuario == 'Anderson' && senha == 1254){
+console.log("")
+var fzr = prompt("Qual a nota do prato?")
+
+if(fzr >= 5){
+
+
+for(var aroz = prompt("Qual foi o prato pedido?"); aroz <= 3; aroz++){console.log(expr)}
+
+  
+
+  var expr = prompt("Voltaria novamente Sim ou Nao??");
+  
+  switch (expr) {
+
+    case 'sim':
+
+     do {
+  console.log("obrigado por jantar conosco")
+  console.log('Volte Sempre!')
+break} while(fzr >= 5)
+
+    break;
+    case 'nao':
+
+    console.log('Lamentamos muito, estamos aprimorando nossos serviços!')
+
+    break
+    }
+}
+
+else if(fzr >= 6){
+
+
+for(var aroz = prompt("Qual foi o prato pedido?"); aroz <= 3; aroz++){console.log(expr)}
+
+  
+
+  var expr = prompt("Voltaria novamente Sim ou Nao??");
+  
+  switch (expr) {
+
+    case 'sim':
+
+     do {
+  console.log("obrigado por jantar conosco")
+  console.log('Volte Sempre!')
+break} while(fzr >= 5)
+
+    break;
+    case 'nao':
+
+    console.log('Lamentamos muito, estamos aprimorando nossos serviços!')
+
+    break
+    }
+
+}
+ 
+break}
+
+  else if(usuario != 'Anderson' && senha != 1254){
+console.log("tente outra vez!")
+}
+}
+
+
+
+
+
+
+
+
+
+
